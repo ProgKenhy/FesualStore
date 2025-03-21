8f2a106dc5c02f0e3a355cb00b7225b4d7cc6886
ProgKenhy <sashok20053@gmail.com>
2025-03-21 20:56:34 +0200

    fix prodict_detail

--- assets/js/product_detail.js
+++ assets/js/product_detail.js
@@ -1,14 +1,17 @@
-function copyProductInfo() {
-    var productInfoField = document.getElementById("productInfoField");
-    productInfoField.select();
-    document.execCommand("copy");
+$(document).ready(function () {
+    // Функция для копирования информации о товаре
+    window.copyProductInfo = function() {
+        var productInfoField = document.getElementById("productInfoField");
+        productInfoField.select();
+        document.execCommand("copy");
 
-    // Изменение текста кнопки для обратной связи
-    var copyButton = document.getElementById("copyButton");
-    copyButton.innerHTML = '<i class="fa fa-check"></i> Скопировано';
+        // Изменение текста кнопки для обратной связи
+        var copyButton = document.getElementById("copyButton");
+        copyButton.innerHTML = '<i class="fas fa-check"></i> Скопировано';
 
-    // Возвращаем оригинальный текст через 2 секунды
-    setTimeout(function () {
-        copyButton.innerHTML = '<i class="fa fa-copy"></i> Копировать';
-    }, 2000);
-}
+        // Возвращаем оригинальный текст через 2 секунды
+        setTimeout(function () {
+            copyButton.innerHTML = '<i class="fas fa-copy"></i> Копировать';
+        }, 2000);
+    };
+});
