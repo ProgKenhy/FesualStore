11f2db192504087f2523d6c1b35ec94bbcf752d0
ProgKenhy <sashok20053@gmail.com>
2025-03-21 20:57:00 +0200

    fix eye toggler

--- assets/js/authorization.js
+++ assets/js/authorization.js
@@ -1,14 +1,22 @@
-function togglePasswordVisibility(passwordFieldId, iconId) {
-    const passwordField = document.getElementById(passwordFieldId);
-    const toggleIcon = document.getElementById(iconId);
+$(document).ready(function () {
+    // Функция для переключения видимости пароля
+    window.togglePasswordVisibility = function(passwordFieldId, iconId) {
+        const passwordField = document.getElementById(passwordFieldId);
+        const toggleIcon = document.getElementById(iconId);
 
-    if (passwordField.type === 'password') {
-        passwordField.type = 'text';
-        toggleIcon.classList.remove('fa-eye');
-        toggleIcon.classList.add('fa-eye-slash');
-    } else {
-        passwordField.type = 'password';
-        toggleIcon.classList.remove('fa-eye-slash');
-        toggleIcon.classList.add('fa-eye');
-    }
-}
+        if (passwordField.type === 'password') {
+            passwordField.type = 'text';
+            toggleIcon.classList.remove('fa-eye');
+            toggleIcon.classList.add('fa-eye-slash');
+        } else {
+            passwordField.type = 'password';
+            toggleIcon.classList.remove('fa-eye-slash');
+            toggleIcon.classList.add('fa-eye');
+        }
+    };
+
+    // Пример использования функции togglePasswordVisibility
+    $('#togglePasswordButton').on('click', function () {
+        togglePasswordVisibility('id_password', 'togglePasswordIcon');
+    });
+});
