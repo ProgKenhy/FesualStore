431045f1defafb8a5e6393268421d7301d80c762
ProgKenhy <sashok20053@gmail.com>
2025-03-21 20:19:29 +0200

    fix error_requests

--- src/config/views.py
+++ src/config/views.py
@@ -1,17 +1,13 @@
 from django.shortcuts import render
 
-
-def bad_request(request):
+def bad_request(request, exception):
     return render(request, 'errors/400.html', status=400)
 
-
-def permission_denied(request):
+def permission_denied(request, exception):
     return render(request, 'errors/403.html', status=403)
 
-
-def page_not_found(request):
+def page_not_found(request, exception):
     return render(request, 'errors/404.html', status=404)
-
 
 def server_error(request):
     return render(request, 'errors/500.html', status=500)
